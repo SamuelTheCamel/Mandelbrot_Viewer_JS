171230cbd696c75adadf608aed92f11ccd8ae019
SamuelTheCamel <samuelvoltz@gmail.com>
2025-01-10 20:12:39 -0500

    Rearranged code to no longer make use of modules

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Mandelbrot Viewer
 This is yet another Mandelbrot Viewer, this time written in JavaScript so that it can be ran on the browser. This Mandelbrot Viewer has some special features that others don't have, including being able to change the recursive formula. Hope you enjoy!
 
 (More information will be included in the HTML document)
+
+Modules used:
+- math.js
--- mandelbrot.js
+++ mandelbrot.js
@@ -1,9 +1,20 @@
 // Module for Mandelbrot Viewer JS
 
-function draw_mb(context, parameters) {
+function DrawParameters(center_x, center_y, scale, max_iter, pixel_size, color_depth, escape_distance) {
+    this.center_x = center_x;
+    this.center_y = center_y;
+    this.scale = scale;
+    this.max_iter = max_iter;
+    this.pixel_size = pixel_size;
+    this.color_depth = color_depth;
+    this.escape_distance = escape_distance;
+};
 
-}
+function drawMandelbrot(context, parameters) {
 
-const mb = {draw_mb:draw_mb};
+};
 
-export default mb;
+function testFunction() {
+    alert(math.evaluate("2+2"));
+};
+
